45101a21b7d1e130e302a6b69fd9695b87f038a3
Syed Muhammad Ammar <syed.ammar.98sep@gmail.com>
2023-08-05 21:12:49 +0500

    logger added. Train.py at initial level

--- src/train.py
+++ src/train.py
@@ -1,1 +1,23 @@
 import argparse
+from config import paths
+from logger import get_logger, log_error
+from schema.data_schema import load_json_data_schema, save_schema
+
+def run_training(
+    input_schema_dir: str = paths.INPUT_SCHEMA_DIR,
+    saved_schema_dir_path: str = paths.SAVED_SCHEMA_DIR_PATH,
+    model_config_file_path: str = paths.MODEL_CONFIG_FILE_PATH,
+    train_dir: str = paths.TRAIN_DIR,
+    preprocessing_config_file_path: str = paths.PREPROCESSING_CONFIG_FILE_PATH,
+    preprocessing_dir_path: str = paths.PREPROCESSING_DIR_PATH,
+    predictor_dir_path: str = paths.PREDICTOR_DIR_PATH,
+    default_hyperparameters_file_path: str = paths.DEFAULT_HYPERPARAMETERS_FILE_PATH,
+    run_tuning: bool = False,
+    ):
+    
+    try:
+        print("Script run successfully")
+    except:
+        pass
+
+run_training()
