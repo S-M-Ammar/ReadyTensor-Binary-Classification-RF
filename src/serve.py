47195b96de10263e7e3bfa4da48ce9281b930b5a
Syed Muhammad Ammar <syed.ammar.98sep@gmail.com>
2023-08-09 00:12:32 +0500

    dat inference validation added

--- src/serve.py
+++ src/serve.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from typing import Any
+import pandas as pd
 import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
+from config import paths
+from data_models.infer_request_model import get_inference_request_body_model
+from logger import log_error
+from serve_utils import (
+    ModelResources,
+    combine_predictions_response_with_explanations,
+    generate_unique_request_id,
+    get_model_resources,
+    logger,
+    transform_req_data_and_make_predictions,
+)
+from xai.explainer import get_explanations_from_explainer
 app = FastAPI()
 
 
